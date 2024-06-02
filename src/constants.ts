edb07dd38027e609378182bc1c1d5a3d692de72e
valerio-pescatori-acn <valerio.pescatori@accenture.com>
2024-06-02 16:00:22 +0200

    init

---
+++ src/constants.ts
@@ -0,0 +1,10 @@
+export const labels = {
+	en: {
+		home: {
+			name: "Valerio Pescatori",
+			jobTitle: "Frontend Engineer",
+			description:
+				"I'm a frontend engineer with 2 years of experience with a passion for code quality. B.Sc. in Computer Science, always trying to learn new things.",
+		},
+	},
+} as const;
---
+++ src/routes/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+	return (
+		<main class="mocha text-center mx-auto text-text min-h-dvh p-4 bg-base">
+			<h1 class="max-6-xs text-6xl font-thin uppercase my-16">Hi </h1>
+		</main>
+	);
+}
