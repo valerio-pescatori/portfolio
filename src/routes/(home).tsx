935b3e754ac1d84df712ba3462cc84b313f92e57
valerio-pescatori-acn <valerio.pescatori@accenture.com>
2024-06-04 11:06:12 +0200

    refactor(home): minor refactor

--- src/routes/(home).tsx
+++ src/routes/(home).tsx
@@ -4,20 +4,18 @@ import { loadLocaleAsync } from "~/i18n/i18n-util.async";
 export default function Home() {
 	const { LL, locale, setLocale } = useI18nContext();
 
+	const updateCookie = async () => {
+		const newLocale = locale() === "en" ? "it" : "en";
+		document.cookie = `lang=${newLocale}; Max-Age=${
+			1000 * 60 * 60 * 24 * 30 * 12
+		}`;
+		await loadLocaleAsync(newLocale);
+		setLocale(newLocale);
+	};
+
 	return (
 		<main class="mocha text-center mx-auto text-text min-h-dvh p-4 bg-base">
-			<button
-				onClick={async () => {
-					const newLocale = locale() === "en" ? "it" : "en";
-					document.cookie = `lang=${newLocale}; Max-Age=${
-						1000 * 60 * 60 * 24 * 30 * 12
-					}`;
-					await loadLocaleAsync(newLocale);
-					setLocale(newLocale);
-				}}
-			>
-				{locale()}
-			</button>
+			<button onClick={updateCookie}>{locale()}</button>
 			<h1 class="max-6-xs text-6xl font-thin uppercase my-16">
 				{LL().hi({ name: "Valerio" })}
 			</h1>
