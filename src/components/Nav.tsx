115b47590a58d8067482a9e85930d35dad0f27fd
valerio-pescatori <pescatorim14@gmail.com>
2024-06-07 22:58:09 +0200

    feat(Nav): adds theme and lang switcher

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,19 +1,29 @@
 import { type FlavorName, flavors } from '@catppuccin/palette';
-import { type Accessor, For, type Setter, Show, createSignal } from 'solid-js';
+import type { Accessor, Setter } from 'solid-js';
+import { useI18nContext } from '~/i18n/i18n-solid';
 import type { Locales } from '~/i18n/i18n-types';
+import { locales } from '~/i18n/i18n-util';
+import { loadLocaleAsync } from '~/i18n/i18n-util.async';
+import Dropdown from './Dropdown';
 
 type NavProps = {
 	theme: Accessor<FlavorName>;
 	setTheme: Setter<FlavorName>;
-	lang: Locales;
 };
 
-export default function Nav({ theme, setTheme, lang }: NavProps) {
-	const [isOpen, setIsOpen] = createSignal(false);
+export default function Nav(props: NavProps) {
+	const { locale, setLocale } = useI18nContext();
+
+	const switchLocale = async (newLocale: Locales) => {
+		document.cookie = `lang=${newLocale}; Max-Age=${
+			1000 * 60 * 60 * 24 * 30 * 12
+		}`;
+		await loadLocaleAsync(newLocale);
+		setLocale(newLocale);
+	};
 
 	const switchTheme = (newTheme: FlavorName) => {
-		setIsOpen(false);
-		setTheme(newTheme);
+		props.setTheme(newTheme);
 		document.cookie = `theme=${newTheme}; Max-Age ${
 			1000 * 60 * 60 * 24 * 30 * 12
 		}`;
@@ -21,32 +31,20 @@ export default function Nav({ theme, setTheme, lang }: NavProps) {
 
 	return (
 		<nav class="bg-mantle flex justify-end px-4 py-2">
-			<div class="relative">
-				<button
-					class="px-4 rounded-xl border border-mauve"
-					type="button"
-					onClick={() => setIsOpen(!isOpen())}
-				>
-					{theme()}
-				</button>
-				<Show when={isOpen()}>
-					<ul class="absolute top-full mt-2 right-0 flex border-mauve border rounded-xl bg-mantle flex-col px-4 py-2">
-						<For each={Object.keys(flavors)}>
-							{(name) => (
-								<li>
-									<button
-										class="w-full text-left"
-										type="button"
-										onClick={() => switchTheme(name as FlavorName)}
-									>
-										{name}
-									</button>
-								</li>
-							)}
-						</For>
-					</ul>
-				</Show>
-			</div>
+			<Dropdown<FlavorName>
+				items={Object.keys(flavors).map((f) => ({
+					value: f as FlavorName,
+					label: f,
+				}))}
+				label={props.theme}
+				class="mr-4"
+				onSelectItem={switchTheme}
+			/>
+			<Dropdown<Locales>
+				label={locale}
+				items={locales.map((v) => ({ label: v, value: v }))}
+				onSelectItem={switchLocale}
+			/>
 		</nav>
 	);
 }
