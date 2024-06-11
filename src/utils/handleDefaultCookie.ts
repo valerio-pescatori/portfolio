a9596a024f78757a758357abf64c6208d6be6247
valerio-pescatori <pescatorim14@gmail.com>
2024-06-11 11:26:23 +0200

    fix: fixes Set-Cookie header

--- src/utils/handleDefaultCookie.ts
+++ src/utils/handleDefaultCookie.ts
@@ -1,5 +1,5 @@
 import type { PageEvent } from '@solidjs/start/server';
-import { getCookie, setHeader } from 'vinxi/http';
+import { appendHeader, getCookie } from 'vinxi/http';
 
 export const handleDefaultCookie = (
 	key: string,
@@ -10,7 +10,7 @@ export const handleDefaultCookie = (
 
 	if (!value) {
 		value = defaultValue;
-		setHeader(
+		appendHeader(
 			event.nativeEvent,
 			'Set-Cookie',
 			`${key}=${value}; Max-Age=${1000 * 60 * 60 * 24 * 30 * 12}`,
