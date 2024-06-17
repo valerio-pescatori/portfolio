41aa0aab096c34565271d65fc83ce371d33432ad
valerio-pescatori <pescatorim14@gmail.com>
2024-06-17 10:34:13 +0200

    feat(TranslateMarkdown): makes component Dynamic
    defaults to `span`

--- src/components/TranslateMarkdown.tsx
+++ src/components/TranslateMarkdown.tsx
@@ -1,16 +1,28 @@
 import DOMPurify from 'isomorphic-dompurify';
 import { marked } from 'marked';
+import { Dynamic } from 'solid-js/web';
 import type { LocalizedString } from 'typesafe-i18n';
 
 type WrapTranslationProps = {
 	message: LocalizedString;
+	as?: keyof HTMLElementTagNameMap;
+	class?: string;
+	classList?: Record<string, boolean>;
 };
 
 export default function TranslateMarkdown(props: WrapTranslationProps) {
-	const parsed = marked.parse(props.message, {
+	const as: keyof HTMLElementTagNameMap = props.as ?? 'span';
+	const parsed = marked.parseInline(props.message, {
 		async: false,
 	}) as string;
 	const message = DOMPurify.sanitize(parsed);
 
-	return <span innerHTML={message} />;
+	return (
+		<Dynamic
+			component={as}
+			innerHTML={message}
+			class={props.class}
+			classList={props.classList}
+		/>
+	);
 }
