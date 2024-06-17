b08e17e7be5d72b7f6cffa6101e2eb5c6ff43eaf
valerio-pescatori <pescatorim14@gmail.com>
2024-06-17 10:36:49 +0200

    feat(Footer): fixed footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
 	return (
-		<footer class="bg-crust text-overlay1 text-right px-4">
+		<footer class="bg-crust text-overlay1 text-right px-4 fixed w-full bottom-0 left-0">
 			&copy; 2024,{' '}
 			<a href="https://github.com/valerio-pescatori">Valerio Pescatori</a>
 		</footer>
