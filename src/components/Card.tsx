74ebcbb01a6fc42bf9f62c6951ef0ff8fbb6a555
valerio-pescatori <pescatorim14@gmail.com>
2024-10-21 15:38:52 +0200

    chore: wip Card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,41 +1,37 @@
-import { createSignal } from 'solid-js';
+import type { Accessor, Setter } from 'solid-js';
 
 export type CardProps = {
 	title: string;
 	description: string;
+	index: Accessor<number>;
+	isActive: boolean;
+	setIsActive: Setter<number | null>;
 };
 
 export default function Card(props: CardProps) {
-	const [isActive, setIsActive] = createSignal(false);
-
-	const cardBody = () => (
-		<>
-			<h2 class="text-lg border-b w-full text-start border-surface1 p-4 not-sr-only">
+	return (
+		<button
+			type="button"
+			falla
+			con
+			js
+			coglione
+			class="bg-surface0 rounded-lg shadow-2xl pb-4 border border-surface1 transition-all text-start absolute flex flex-col"
+			classList={{
+				'h-[80dvh] z-10 bottom-4': props.isActive,
+				'h-32 hover:scale-105 active:scale-95': !props.isActive,
+			}}
+			style={{
+				top: props.isActive ? '1rem' : `${props.index() * 10}rem`,
+			}}
+			onClick={() => props.setIsActive(props.isActive ? null : props.index())}
+		>
+			<h2 class="text-lg border-b w-full border-surface1 p-4 not-sr-only">
 				{props.title}
 			</h2>
 			<div class="px-4 pt-2">
 				<p>{props.description}</p>
 			</div>
-		</>
-	);
-
-	const cl =
-		'bg-surface0 rounded-lg shadow-2xl pb-4 border border-surface1 transition';
-
-	return (
-		<div class={`${cl} hover:scale-105 active:scale-95 relative`}>
-			{/* placeholder to maintain the position in the page */}
-			<div aria-hidden>{cardBody()}</div>
-			<button
-				type="button"
-				class={`${cl} text-start absolute top-0 left-0 w-full`}
-				classList={{
-					'h-[80dvh] z-20': isActive(),
-				}}
-				onClick={() => setIsActive(!isActive())}
-			>
-				{cardBody()}
-			</button>
-		</div>
+		</button>
 	);
 }
--- src/routes/thingsivedone/(thingsivedone).tsx
+++ src/routes/thingsivedone/(thingsivedone).tsx
@@ -1,4 +1,4 @@
-import { For } from 'solid-js';
+import { For, createSignal } from 'solid-js';
 import Card from '../../components/Card';
 import Typewriter from '../../components/Typewriter/TypewriterIsolated';
 import { typewriterFast } from '../../components/Typewriter/constants/typewriterConfigs';
@@ -8,6 +8,9 @@ import { useIsVisited } from '../../utils/useIsVisited';
 export default function ThingsIveDone() {
 	const { LL } = useI18nContext();
 	const isVisited = useIsVisited();
+	const [currentActiveCard, setCurrentActiveCard] = createSignal<number | null>(
+		null,
+	);
 
 	const cards = Object.values(LL().thingsivedone.cards);
 
@@ -20,10 +23,16 @@ export default function ThingsIveDone() {
 					drawCharRandomness={typewriterFast}
 				/>
 			</h1>
-			<div class="mt-16 flex flex-col gap-8">
+			<div class="mt-16 relative">
 				<For each={cards}>
-					{(card) => (
-						<Card title={card.title()} description={card.description()} />
+					{(card, i) => (
+						<Card
+							title={card.title()}
+							index={i}
+							description={card.description()}
+							isActive={currentActiveCard() === i()}
+							setIsActive={setCurrentActiveCard}
+						/>
 					)}
 				</For>
 			</div>
