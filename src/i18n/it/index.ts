503b1348e37318a12f028e6f503a28ced00e0725
valerio-pescatori <pescatorim14@gmail.com>
2024-10-25 16:26:12 +0200

    feat(whoami): adding cards content

--- src/i18n/it/index.ts
+++ src/i18n/it/index.ts
@@ -28,8 +28,18 @@ const it = {
 		dialogAriaLabel: 'Dettaglio card: {title}',
 		cards: [
 			{
-				title: 'Portfolio',
-				description: 'test',
+				title: 'Portfolio personale',
+				description: `
+				In seguito all'esperienza fatta in ambito professionale ho deciso di ri-sviluppare il mio portfolio, includendo nuove tecnologie per implementare un'architettura robusta, pulita, orientata all'automazione.
+				
+				Inanzitutto ho voluto testare un nuovo meta-framework: **SolidStart**. Nato da una libreria che ho amato fin subito per il suo approccio innovativo che ha rivoluzionato l'ecosistema JavaScript mondiale attraverso l'utilizzo degli ormai famosi _signals_ e attraverso la rimozione di sovrastrutture spesso non necessarie come il Virtual DOM.
+
+				Per l'aspetto CSS ho utilizzato **TailwindCSS**, sicuramente tra i framework migliori per la gestione dello stile, comodissimo con le sue classi di utility e leggerissimo grazie all'utilizzo di PurgeCSS per spedire solo il CSS necessario al render della pagina.
+
+				Per tenere il codice pulito e ben organizzato ho utilizzato **BiomeJS**, un tool all-in-one scritto in _Rust_ che sostituisce in un colpo solo sia ESLint che Prettier, in quando agisce sia da linter che da formatter. Ho voluto provare Biome per testare con mano la velocità di Rust.
+
+				Come git hook manager ho usato **Lefthook**, tool innovativo scritto in GO, facilissimo da configurare e velocissimo nell'esecuzione delle pipeline, accoppiato a Biome permette di eseguire linting e formatting al commit in pochissimi millisecondi.
+				`,
 			},
 			{
 				title: 'Portfolio',
