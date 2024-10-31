2307a7b6d0379af846bd165e26394ee640f33dc0
valerio-pescatori <pescatorim14@gmail.com>
2024-10-31 17:59:01 +0100

    feat(Card): adds gradient to hide overflowing text
    other minor refactors

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -11,34 +11,37 @@ export type CardProps = {
 export default function Card(props: CardProps) {
 	const [isActive, setIsActive] = createSignal(false);
 
-	const cardBody = (hasMaxH = false) => (
+	const cardBody = () => (
 		<TranslateMarkdown
-			class="px-4 pt-2 min-h-12"
-			classList={{ 'max-h-48 overflow-hidden': hasMaxH }} // add ellipsis
+			class="px-4 pt-2 min-h-16"
 			message={props.description()}
 			as="p"
 		/>
 	);
 
 	return (
-		<div
-			class={
-				'bg-surface0 rounded-lg border border-surface1 transition-all duration-500 shadow-2xl'
-			}
-		>
-			<button
-				class="text-start hover:ring-opacity-100 focus:ring-opacity-100 transition-shadow ring-1 ring-opacity-0 ring-mauve rounded-lg w-full align-top"
-				type="button"
-				onClick={() => setIsActive(true)}
-			>
-				<h2 class="text-lg border-b w-full border-surface1 p-4 flex justify-between">
-					{props.title()}
-				</h2>
-				{cardBody(true)}
-			</button>
+		<>
 			<Modal title={props.title} isOpen={isActive} setIsOpen={setIsActive}>
 				{cardBody()}
 			</Modal>
-		</div>
+			<div class="bg-surface0 rounded-lg border border-surface1 transition-transform shadow-2xl hover:scale-105">
+				<button
+					class={`
+					text-start 
+					hover:ring-opacity-100 focus:ring-opacity-100 transition-shadow ring-1 ring-opacity-0 ring-mauve
+					rounded-lg w-full align-top`}
+					type="button"
+					onClick={() => setIsActive(true)}
+				>
+					<h2 class="text-lg border-b w-full border-surface1 p-4 flex justify-between">
+						{props.title()}
+					</h2>
+					<div class="max-h-48 overflow-hidden relative">
+						<div class="absolute size-full rounded-b-lg bg-gradient-to-t from-surface0 to-transparent to-60%" />
+						{cardBody()}
+					</div>
+				</button>
+			</div>
+		</>
 	);
 }
