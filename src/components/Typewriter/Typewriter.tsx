d23df5b4727480a7389244034a056a2e7175abe1
valerio-pescatori <pescatorim14@gmail.com>
2025-11-09 18:13:20 +0100

    fix(Typewriter): fixes lang change bug

--- src/components/Typewriter/Typewriter.tsx
+++ src/components/Typewriter/Typewriter.tsx
@@ -1,3 +1,85 @@
-import { clientOnly } from '@solidjs/start';
+import { Show, createEffect, createSignal, onCleanup } from 'solid-js';
+import type { DrawCharRandomness } from './types/DrawCharRandomness';
+import { generateActions } from './utils/generateActions';
+import { sleep } from './utils/sleep';
 
-export default clientOnly(() => import('./TypewriterIsolated'));
+type TypewriterProps = {
+	text: string;
+	disableAnimation?: boolean;
+	onAnimationEnd?: () => void;
+	drawCharRandomness?: DrawCharRandomness;
+	showCursor?: boolean;
+};
+
+export default function Typewriter(props: TypewriterProps) {
+	const [content, setContent] = createSignal('');
+	const [showCursor, setShowCursor] = createSignal(false);
+	const [actions, setActions] = createSignal(
+		generateActions(props.text, props.drawCharRandomness),
+	);
+	props.showCursor ??= true;
+	let timeout: NodeJS.Timeout | null = null;
+
+	// blinking cursor
+	createEffect(() => {
+		if (!props.showCursor || props.disableAnimation) return;
+		if (props.text !== content()) {
+			setShowCursor(true);
+		} else {
+			timeout = setInterval(() => {
+				setShowCursor(!showCursor());
+			}, 350);
+
+			setTimeout(() => {
+				timeout && clearTimeout(timeout);
+				setShowCursor(false);
+			}, 2000);
+		}
+	});
+
+	// ciclo di scrittura
+	createEffect(() => {
+		let cancelled = false;
+
+		onCleanup(() => {
+			cancelled = true;
+			setContent('');
+			setActions(generateActions(props.text, props.drawCharRandomness));
+		});
+
+		(async () => {
+			// forza la dipendenza su props.text
+			props.text;
+
+			for (const action of await actions()) {
+				if (cancelled) break; // se il cleanup è stato chiamato, interrompi subito
+
+				await sleep(action.time);
+				if (cancelled) break;
+
+				if (action.type === 'add') {
+					setContent((c) => c + action.char);
+				} else {
+					setContent((c) => c.slice(0, -1));
+				}
+			}
+
+			if (!cancelled) {
+				props.onAnimationEnd?.();
+			}
+		})();
+	});
+
+	return (
+		<>
+			<span class="sr-only">{props.text}</span>
+			<span
+				aria-busy={!props.disableAnimation && content() !== props.text}
+				aria-hidden
+			>
+				{props.disableAnimation ? props.text : content()}
+				<Show when={showCursor()}>|</Show>
+			</span>
+		</>
+	);
+}
---
+++ src/components/Typewriter/utils/generateActions.ts
@@ -0,0 +1,74 @@
+import { typoMap } from '../constants/typoMap';
+import type { DrawCharRandomness } from '../types/DrawCharRandomness';
+import type { MistypableCharacters } from '../types/alphabet';
+
+export type WriteAction =
+	| {
+			type: 'add';
+			char: string;
+			time: number;
+	  }
+	| {
+			type: 'delete';
+			time: number;
+	  };
+
+export const generateActions = async (
+	text: string,
+	drawCharRandomness?: DrawCharRandomness,
+) => {
+	'use server';
+	const actions: WriteAction[] = [];
+	// generate string
+	let isComplete = false;
+	let currentStr = '';
+	const typoRate = drawCharRandomness?.typoRate ?? 0.2;
+	const maxTimeout = drawCharRandomness?.maxTimeout ?? 250;
+	const deleteTimeoutMultiplier =
+		drawCharRandomness?.deleteTimeoutMultiplier ?? 1.2;
+
+	while (!isComplete) {
+		/** indice del cursore */
+		const index = currentStr.length;
+		/** carattere corretto da scrivere */
+		const char = text[index];
+
+		// generate an action
+		const isError = Math.random() < typoRate;
+
+		// se è error aggiungo 3 azioni -> errore, eliminazione e correzione.
+		// altrimenti una sola azione -> scrittura corretta
+		const typo = typoMap[char as MistypableCharacters];
+		if (isError && typo) {
+			const typoChar = typo[Math.floor(Math.random() * typo.length)];
+			const typoTime = Math.random() * maxTimeout * deleteTimeoutMultiplier;
+			const correctionTime =
+				Math.random() *
+				maxTimeout *
+				deleteTimeoutMultiplier *
+				deleteTimeoutMultiplier;
+
+			// first action: error
+			actions.push({
+				type: 'add',
+				char: typoChar,
+				time: typoTime,
+			});
+
+			// second actions: error cancellation
+			actions.push({
+				type: 'delete',
+				time: correctionTime,
+			});
+		}
+		// third (or first) action: the correct one
+		actions.push({
+			type: 'add',
+			time: Math.random() * maxTimeout,
+			char,
+		});
+		currentStr += char;
+		isComplete = currentStr === text;
+	}
+	return actions;
+};
